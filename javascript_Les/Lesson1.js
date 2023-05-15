914f1b0a075cc6ff881c82e50ad5d2f2a0d554db
Darien Edwards <edwards.darien@outlook.com>
2023-05-15 18:27:09 -0400

    Fcc JavaScript Start

---
+++ javascript_Les/Lesson1.js
@@ -0,0 +1,7 @@
+// The thing i've been asked to do is comment out some code.
+
+/* The previous code was for single line comments
+this one is for multi line comments */
+
+// Next we are going to name a variable
+var myName;
