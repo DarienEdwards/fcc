87043ba9e918b0123b3ab6aa9a4068ee353824c3
Darien Edwards <edwards.darien@outlook.com>
2023-05-18 13:20:23 -0400

    More FCC coding challenges -

--- javascript_Les/Lesson1.js
+++ javascript_Les/Lesson1.js
@@ -19,3 +19,8 @@ properCamelCase = "A String";
 titleCaseOver = 9000;
 
 // Test Typing to see if anything changes test test test
+
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
