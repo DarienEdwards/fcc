e5446300aa4483ed7399821267dd872e53080acc
Darien Edwards <edwards.darien@outlook.com>
2023-06-03 23:30:35 -0400

    Making note of array, starting to make sense

--- javascript_Les/Lesson1.js
+++ javascript_Les/Lesson1.js
@@ -40,3 +40,13 @@ const someAdjective = "Amazing";
 let myStr = "Learning to code is ";
 
 myStr += someAdjective;
+
+// This use to confuse me really bad
+const myArray = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [[10, 11, 12], 13, 14],
+  ];
+  
+  const myData = myArray[2][1];
