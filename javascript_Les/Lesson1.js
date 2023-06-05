14eeb8cb6c6b1aa7faacdeb12f15f82be8f46ae5
Darien Edwards <edwards.darien@outlook.com>
2023-06-05 16:58:33 -0400

    JavaScript

--- javascript_Les/Lesson1.js
+++ javascript_Les/Lesson1.js
@@ -51,3 +51,15 @@ const myArray = [
   
   const answer = timesFive(5);
   
+  // Setup
+const outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+  const outerWear = "sweater";
+  
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
