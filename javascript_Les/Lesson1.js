8335fd8573fa8bddf0a428273c59ed4c8ad7d93d
Darien Edwards <edwards.darien@outlook.com>
2023-06-06 22:34:14 -0400

    JS for the day

--- javascript_Les/Lesson1.js
+++ javascript_Les/Lesson1.js
@@ -73,3 +73,13 @@ function testEqual(val) {
   }
   
   testEqual(10);
+
+  // Setup
+function testNotEqual(val) {
+    if (val != 99) { // Change this line
+      return "Not Equal";
+    }
+    return "Equal";
+  }
+  
+  testNotEqual(10);
