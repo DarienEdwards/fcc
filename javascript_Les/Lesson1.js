8b167e10bc637dc1f676ea777568d55343937e99
Darien Edwards <edwards.darien@outlook.com>
2023-06-09 15:48:43 -0400

    JS practice

--- javascript_Les/Lesson1.js
+++ javascript_Les/Lesson1.js
@@ -244,3 +244,119 @@ function testSize(num) {
   }
   
   switchOfStuff(1);
+
+  // multiple switch cases with no break
+
+  function sequentialSizes(val) {
+    let answer = "";
+    // Only change code below this line
+  
+  switch(val) {
+    case 1:
+    case 2:
+    case 3:
+      answer = "Low";
+      break;
+    case 4:
+    case 5:
+    case 6:
+      answer = "Mid";
+      break;
+    case 7:
+    case 8:
+    case 9:
+      answer = "High";
+      break;
+  }
+  
+  
+    // Only change code above this line
+    return answer;
+  }
+  
+  sequentialSizes(1);
+
+  function chainToSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+
+
+  switch (val) {
+    case "bob":
+    answer = "Marley";
+    break;
+    case 42:
+    answer = "The Answer";
+    break;
+    case 1:
+    answer = "There is no #1";
+    break;
+    case 99:
+    answer = "Missed me by this much!";
+    break;
+    case 7:
+    answer = "Ate Nine";
+
+
+  }
+
+  // Only change code above this line
+  return answer;
+}
+
+chainToSwitch(7);
+
+// Instead of else if, you can use switch, see below
+
+function chainToSwitch(val) {
+    let answer = "";
+    // Only change code below this line
+  
+    /* 
+
+      if (val === "bob") {
+    answer = "Marley";
+  } else if (val === 42) {
+    answer = "The Answer";
+  } else if (val === 1) {
+    answer = "There is no #1";
+  } else if (val === 99) {
+    answer = "Missed me by this much!";
+  } else if (val === 7) {
+    answer = "Ate Nine";
+  }
+
+  */
+  function chainToSwitch(val) {
+    let answer = "";
+    // Only change code below this line
+  
+   switch (val) {
+     case "bob":
+     answer = "Marley";
+     break;
+     case 42:
+     answer = "The Answer";
+     break;
+     case 1:
+     answer = "There is no #1";
+     break;
+     case 99:
+     answer = "Missed me by this much!";
+     break;
+     case 7:
+     answer = "Ate Nine";
+     break;
+     case "John":
+     answer = "";
+     break;
+     case 156:
+     answer = "";
+     break;
+   }
+  
+    // Only change code above this line
+    return answer;
+  }
+  
+  chainToSwitch(7);
